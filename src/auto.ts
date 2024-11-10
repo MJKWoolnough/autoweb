462f74d765c25a415b8a7e6fbe36678f6b98d6d6
Michael Woolnough <michael.woolnough@gmail.com>
2024-11-10 18:38:00 +0000

    restructured Control to just be an object.

--- src/auto.ts
+++ src/auto.ts
@@ -3,30 +3,23 @@ import ready from './lib/load.js';
 import {queue} from './lib/misc.js';
 import {RPC} from './lib/rpc.js';
 
-class ControlClass {
-	#rpc: RPC;
-
-	constructor(rpc: RPC) {
-		this.#rpc = rpc;
-	}
-}
-
-export type Control = InstanceType<typeof ControlClass>;
-
 queue(() => ready);
 
-const f = fetch;
+let rpc: RPC | null = null;
+
+const f = fetch,
+      control = Object.freeze({});
 
 window.WebSocket = class extends WebSocket{};
 window.XMLHttpRequest = class extends XMLHttpRequest{};
 window.fetch = (input: RequestInfo | URL, init?: RequestInit): Promise<Response> => f(input, init);
 
-export default (url: string, fn: (c: Control) => Promise<void> ) => {
+export default (url: string, fn: (c: typeof control) => Promise<void> ) => {
 	return queue(() => WS("/socket").then(ws => {
-		const rpc = new RPC(ws);
+		rpc = new RPC(ws);
 
 		return rpc.request("proxy", url)
-		.then(() => fn(new ControlClass(rpc)))
-		.finally(() => rpc.close());
+		.then(() => fn(control))
+		.finally(() => rpc?.close());
 	}));
 }
