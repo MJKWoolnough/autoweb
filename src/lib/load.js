23a3126e4253e0e2f433e49e9a63710976f5a759
Michael Woolnough <michael.woolnough@gmail.com>
2024-11-17 19:23:12 +0000

    Removed unnecessary JS files.

--- src/lib/load.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * The load module contains a single default export, a {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise | Promise} which is resolved when the page finished loading.
- * @module load
- */
-/** */
-Object.defineProperty(exports, "__esModule", { value: true });
-/** This Promise successfully resolves when the page is loaded. */
-exports.default = document.readyState === "complete" ? Promise.resolve() : new Promise(function (successFn) { return window.addEventListener("load", successFn, { "once": true }); });
